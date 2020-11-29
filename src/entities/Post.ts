fb0bfa7a7b54ff8860642306821543408c324311
timodie <timodie.addai@gmail.com>
2020-11-28 19:36:13 -0500

    setting up mikro-orm

---
+++ src/entities/Post.ts
@@ -0,0 +1,18 @@
+import { Entity, EntitySchema PrimaryKey, Property} from '@mikro-orm/core'
+
+@Entity()
+export class Post {
+
+  @PrimaryKey()
+  id!: number;
+
+  @Property({ type: 'date'})
+  createdAt = new Date();
+
+  @Property({type: 'date', onUpdate: () => new Date() })
+  updatedAt = new Date();
+
+  @Property( {type: 'text' })
+  title!: string;
+
+}
