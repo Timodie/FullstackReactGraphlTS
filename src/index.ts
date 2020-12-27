730c6bdbed208a4c44540b12dd2456fa6192092d
timodie <timodie.addai@gmail.com>
2020-12-26 21:30:40 -0500

    hardcode cors origin

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,11 @@ import { PostResolver } from './resolvers/post';
 import { UserResolver } from './resolvers/user';
 import redis from 'redis';
 import session from 'express-session';
-import connectRedis from 'connect-redis';
+import connectRedis from 'connect-redis'
 import { __prod__ } from './constants';
 import { MyContext } from './types';
+
+
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -22,37 +24,49 @@ const main = async () => {
   }
 
   const app = express();
-
-  const RedisStore = connectRedis(session);
-  const redisClient = redis.createClient();
+   
+  const RedisStore = connectRedis(session)
+  const redisClient = redis.createClient()
   // session middleware should init before apollo because we'll use it in apollo
+  /**
+   * How sessions work
+   * 1. login : userId gets stored in redis e.g. sess:qwreqwewqewwqe -> { userId: 1}
+   * 2. express-session sets cookie on browser
+   * 3. user makes a request with the cookie included in the req header
+   * 4. cookie is decrypted using secret
+   * 5. request is made to redis to retrieve decryped key and then stored in session
+   */
   app.use(
-    session({
-      name: 'learnGraphQLCookie',
-      store: new RedisStore({
-        client: redisClient,
-        disableTouch: true // make cookies last forever , reduce requests to redis
-      }),
-      cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 365,
-        httpOnly: true,
-        secure: __prod__,
-        sameSite: 'lax' // protects csrf TODO: read more about CSRF
-      },
-      saveUninitialized: false,
-      secret: 'changethistosmethingelsepassviaenvVariables',
-      resave: false
-    })
-  );
+  session({
+    name:"learnGraphQLCookie",
+    store: new RedisStore({ 
+      client: redisClient,
+      disableTouch: true // make cookies last forever , reduce requests to redis
+  
+    }),
+    cookie:{
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+      httpOnly: true,
+      secure: __prod__,
+      sameSite: 'lax', // protects csrf TODO: read more about CSRF
+    },
+    saveUninitialized: false,
+    secret: 'changethistosmethingelsepassviaenvVariables',
+    resave: false,
+    
+  })
+  )
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false
     }),
-    context: ({ req, res }): MyContext => ({ em: orm.em, req, res })
+    context: ({req, res}): MyContext => ({ em: orm.em, req, res})
   });
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({ app, 
+  cors: { origin: "http://localhost:3000" }  // Access-Control-Allow-Origin must not be wildcard * ,
+ });
   app.listen(4000, () => {
     console.log('GraphQL server started on localhost:4000');
   });
